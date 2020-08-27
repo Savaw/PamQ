82eb09d7777c73732b2324f03f86cb1e19a721d2
savaw <savaw97@gmail.com>
2020-08-27 23:08:02 +0430

    Edit signup handler

--- main.go
+++ main.go
@@ -23,10 +23,10 @@ type User struct {
 }
 
 type NewUser struct {
-	Username		string
-	Email			string
-	Password		string
-	PasswordConfirm string
+	Username		string	`json:"username"`
+	Email			string	`json:"email"`
+	Password		string	`json:"password"`
+	PasswordConfirm string	`json:"password_confirm"`
 }
 
 type ErrorMissingField string
@@ -35,7 +35,7 @@ func (e ErrorMissingField) Error() string {
 	return string(e) + " is required."
 }
 
-func (u *NewUser) validate() error {
+func (u *NewUser) Validate() error {
 	if len(u.Username) == 0 {
 		return ErrorMissingField("Username")
 	}
@@ -54,6 +54,36 @@ func (u *NewUser) validate() error {
 	return nil		
 }
 
+func (u * NewUser) CreateUser() (*User, error) {
+	user := &User{
+		Username: u.Username,
+		Email: u.Email,
+		Password: ("SomeSaltHereMaybeThere" + u.Password)}
+
+	//todo add to db
+
+	return user, nil
+}
+
+func returnAsJson(w http.ResponseWriter, mp map [string]interface{}) {
+	js, err2 := json.Marshal(mp); 
+
+	if err2 != nil {
+		http.Error(w, err2.Error(), http.StatusInternalServerError)
+		return
+	  }
+	w.Write(js)
+}
+
+func returnErrorAsJson(w http.ResponseWriter, err string) {
+	returnAsJson(w, map [string]interface{}{"error":err})
+}
+
+func returnMessageAsJson(w http.ResponseWriter, msg string) {
+	returnAsJson(w, map [string]interface{}{"message":msg})
+}
+
+
 func signupPostHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	// var user User 
@@ -64,28 +94,24 @@ func signupPostHandler(w http.ResponseWriter, r *http.Request) {
 		log.Print(err)
 		// fmt.Fprintf(w,`{"error": "Error decoding json. (%s)"}`, err )
 
-		// js, err2 := json.Marshal(fmt.Sprintf(`{"error": "Error decoding json. (%s)"}`, err)); 
-		js, err2 := json.Marshal(map [string]string{"error":fmt.Sprintf(`Error decoding JSON. (%s)`, err)}); 
-
-		if err2 != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		  }
-		w.Write(js)
+		returnErrorAsJson(w, fmt.Sprintf(`Error decoding JSON. (%s)`, err))
 		return
 	}
 
-	if err := newUser.validate(); err != nil {
-		js, err2 := json.Marshal(map [string]string{"error": fmt.Sprintf("%s",err)}); 
-
-		if err2 != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		  }
-		w.Write(js)
+	if err := newUser.Validate(); err != nil {
+		returnErrorAsJson(w, fmt.Sprintf("%s",err))
 		return
 	}
 
+	var user *User
+	var err error
+	if user, err = newUser.CreateUser(); err != nil {
+		returnErrorAsJson(w, fmt.Sprintf("%s",err))
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	returnMessageAsJson(w, fmt.Sprintf(`User %s created.`, user.Username))
+	return
 }
 
 func main() {
